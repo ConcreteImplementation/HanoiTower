9cc5a514ac8ffc0baf2a9c8e922186dd318d3926
ConcreteImplementation <carambole.star@gmail.com>
2021-05-23 08:26:36 -0400

    Files import

---
+++ HanoiTower/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace HanoiTower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int disks = 7;
+            //int wait = 500;
+            //TowerDescribe tower = new TowerDescribe(disks, wait);
+            //tower.Solve();
+
+            TicksCount();
+        }
+
+
+        static void TicksCount()
+        {
+            Stopwatch chronometre = new Stopwatch();
+            long tick = 0;
+            
+            StringBuilder csv = new StringBuilder("NbDisks,Ticks\n");
+
+            for (int disks = 1; disks <= 20; disks++)
+            {
+                Console.Write($"Solving {disks,-4} disks");
+
+                Tower tower = new Tower(disks);
+
+                chronometre.Restart();
+                tower.Solve();
+                chronometre.Stop();
+
+                tick = chronometre.ElapsedTicks;
+
+                csv.Append($"{disks},{tick}\n");
+
+                Console.WriteLine($"\t{tick,-10}");
+            }
+
+            
+            StreamWriter csvFile = new StreamWriter($"TickCount-{DateTime.Now.ToString("dd-MM-yyyy--hh-mm-ss")}.csv");
+            csvFile.Write(csv.ToString());
+            csvFile.Close();
+        }
+
+
+
+
+    }
+}
---
+++ HanoiTower/TowerDescribe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using System.Threading;
+
+namespace HanoiTower
+{
+    class TowerDescribe : Tower
+    {
+        int wait;
+        public TowerDescribe(int nbDisks, int wait)
+            :base(nbDisks)
+        { 
+            this.wait = wait; 
+        }
+
+
+        protected override void Hook()
+        {
+            Console.Clear();
+            Console.WriteLine(this);
+
+            if(Console.KeyAvailable)
+            {
+                Console.ReadKey(false);
+                Console.WriteLine("P A U S E D ");
+                Console.ReadKey(false);
+            }
+
+            Thread.Sleep(wait);
+        }
+
+    }
+}
